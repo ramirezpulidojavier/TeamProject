2c8a7a249995871187d5bc8e40af4ec61c063b68
Cuadrado Gracia <juacuadr@t-systems.es>
2022-10-17 10:42:27 +0200

    uploading test Connection

---
+++ ChatClient/src/test/java/com/curso/chatclient/ConnectionTest.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.curso.chatclient;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author juacuadr
+ */
+public class ConnectionTest {
+    
+    public ConnectionTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        fail("The test case is a prototype.");
+    }
+    
+}
