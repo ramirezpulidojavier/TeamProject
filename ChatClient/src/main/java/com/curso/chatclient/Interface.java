495e0b19110d3fa020e92970a4bdbeac937ae426
Rus Manjon <jrusmanj@t-systems.es>
2022-10-17 11:18:03 +0200

    Interface v1

--- ChatClient/src/main/java/com/curso/chatclient/Interface.java
+++ ChatClient/src/main/java/com/curso/chatclient/Interface.java
@@ -4,10 +4,30 @@
  */
 package com.curso.chatclient;
 
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 /**
  *
  * @author jramir14
  */
 public class Interface {
-    
+
+    public void run() {
+        boolean running = true;
+        Scanner sc = new Scanner(System.in);
+        String msg;
+
+        System.out.println("Welcome to T-Sysgram.");
+
+        while (running) {
+            try {
+                System.out.println("Introduce your message.");
+                msg = sc.next();
+            } catch (NoSuchElementException e) {
+                System.err.println(e);
+            }
+
+        }
+    }
 }
