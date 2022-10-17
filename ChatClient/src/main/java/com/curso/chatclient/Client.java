6e0ea4cf9dff9c308dc154db27600a40bf2b85a4
Corrales Garcia <pcorrale@t-systems.es>
2022-10-17 14:20:45 +0200

    added date in sendMessage method and javadoc in Client class

--- ChatClient/src/main/java/com/curso/chatclient/Client.java
+++ ChatClient/src/main/java/com/curso/chatclient/Client.java
@@ -10,9 +10,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
- *
+ * Class Client with the methods: sendMessage and getMessage
  * @author pcorrales2010
  */
 public class Client {
@@ -20,11 +25,25 @@ public class Client {
     public Client() {
     }
 
+    /**
+     * Method 
+     * @param message The string you want to sent + current date
+     * @param socket Server's socket
+     * @throws IOException
+     */
     public void sendMessage(String message, Socket socket) throws IOException {
+        LocalDateTime date = LocalDateTime.now();  
+        message = message + " " + date.toString();
         PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
         writer.println(message);
     }
 
+    /**
+     *  Method
+     * @param socket
+     * @return Message sent by server
+     * @throws IOException
+     */
     public String getMessage(Socket socket) throws IOException {
         InputStream input = socket.getInputStream();
         BufferedReader reader = new BufferedReader(new InputStreamReader(input));
