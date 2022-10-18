b6fa5ceed999a37442377338122d668c3032ad21
Cuadrado Gracia <juacuadr@t-systems.es>
2022-10-18 09:49:41 +0200

    Modified Client constructor and some comments has been added

--- ChatClient/src/main/java/com/curso/chatclient/Client.java
+++ ChatClient/src/main/java/com/curso/chatclient/Client.java
@@ -14,9 +14,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.time.LocalDateTime;
 
-
 /**
  * Class Client with the methods: sendMessage and getMessage
+ *
  * @author pcorrales2010
  */
 public class Client {
@@ -25,39 +25,53 @@ public class Client {
     PrintWriter myWriter;
     BufferedReader myReader;
 
-    public Client(Socket newSocket) {
-        mySocket = newSocket;
-        try {
-            myWriter = new PrintWriter(newSocket.getOutputStream(), true);
-        } catch (IOException ex) {
-            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        InputStream input;
-        try {
-            input = mySocket.getInputStream();
-            myReader = new BufferedReader(new InputStreamReader(input));
-        } catch (IOException ex) {
-            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+    /**
+     * Constructor that receive a Socket and fill myWriter and myReader private variables.
+     * 
+     * @param newSocket
+     * @throws IOException when an input output error is detected
+     */
+    public Client(Socket newSocket) throws IOException {
+        if (newSocket != null) {
+            mySocket = newSocket;
+            try {
+                InputStream input;
+                myWriter = new PrintWriter(newSocket.getOutputStream(), true);
+                input = mySocket.getInputStream();
+                myReader = new BufferedReader(new InputStreamReader(input));
+            } catch (IOException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, "IOException occurred", ex);
+                throw ex;
+            }
         }
     }
-
-
+    
+    /**
+     * Parametrized constructor to inject variables. This constructor is being used into Test files.
+     * 
+     * @param newSocket
+     * @param newWriter
+     * @param newReader 
+     */
     public Client(Socket newSocket, PrintWriter newWriter, BufferedReader newReader) {
         mySocket = newSocket;
         myWriter = newWriter;
         myReader = newReader;
     }
+
     /**
      * Send the message and current date from client to server
+     *
      * @param message The message to send to server
      * @throws IOException when input output error is detected.
      */
     public void sendMessage(String message) throws IOException {
         myWriter.println(message);
     }
-    
+
     /**
      * Get the message from server
+     *
      * @return Message sent by server
      * @throws IOException when input output error is detected.
      */
