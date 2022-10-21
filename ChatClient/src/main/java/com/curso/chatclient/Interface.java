06054fdba2e9000f70786c72c1486b0230cd6f02
Ruiz Talavera <gruiztal@t-systems.es>
2022-10-21 14:49:31 +0200

    Add dates to messages and preparation for encription

--- ChatClient/src/main/java/com/curso/chatclient/Interface.java
+++ ChatClient/src/main/java/com/curso/chatclient/Interface.java
@@ -66,7 +66,8 @@ public class Interface {
                 listener.interrupt();
 
             } else {
-                sender.sendMessage(msg);
+                //sender.sendMessage(msg);
+                sender.sendChatMessage(msg);
             }
 
         }
